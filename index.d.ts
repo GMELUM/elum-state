65cf95c4cc81e3d3d78c8c656aec1ebb8e50cd50
gmelum <gmelum@my.com>
2022-08-05 12:50:46 +0300

    v0.1.0

--- index.d.ts
+++ index.d.ts
@@ -1,29 +1,18 @@
-import { FC, HTMLAttributes } from "react";
-declare type EventType = string | symbol;
-declare type Handler<T = unknown> = (event: T) => void;
-declare type EventHandlerList<T = unknown> = Array<Handler<T>>;
-declare type EventHandlerMap<Events extends Record<EventType, unknown>> = Map<keyof Events, EventHandlerList<Events[keyof Events]>>;
+import { Dispatch } from "react";
 declare type Atom<T> = {
     key: string;
     default: T;
 };
-interface RootStateProps extends HTMLAttributes<HTMLDivElement> {
-}
-declare const emmiter: <Events extends Record<EventType, unknown>>(all?: EventHandlerMap<Events>) => {
-    all: EventHandlerMap<Events>;
-    on: <Key extends keyof Events>(type: Key, handler: Handler<Events[keyof Events]>) => void;
-    off: <Key_1 extends keyof Events>(type: Key_1, handler?: Handler<Events[keyof Events]> | undefined) => void;
-    emit: <Key_2 extends keyof Events>(type: Key_2, evt?: Events[Key_2] | undefined) => void;
+declare type GlobalAtom<T> = {
+    key: string;
+    default: T;
+    get: () => T;
+    set: (value: any) => void;
+    sub: (handle: Dispatch<T>) => void;
 };
-declare const atom: <T>(opt: Atom<T>) => Atom<T>;
-declare const useFreeGlobalState: <T>(atom: Atom<T>) => [{
+export declare const atom: <T>(opt: Atom<T>) => GlobalAtom<T>, useGlobalValue: <T>(state: GlobalAtom<T>) => T, useSetGlobalState: <T>(state: GlobalAtom<T>) => (value: T) => void, useGlobalState: <T>(state: GlobalAtom<T>) => [T, (value: T) => void], useFreeGlobalState: <T>(state: GlobalAtom<T>) => [{
     current: T;
-}, (value: T) => void];
-declare const useGlobalState: <T>(atom: Atom<T>) => [T, (value: T) => void];
-declare const useGlobalUnSubscribe: <T>(atom: Atom<T>) => {
+}, (value: T) => void], useGlobalUnSubscribe: <T>(state: GlobalAtom<T>) => {
     current: T;
 };
-declare const useGlobalValue: <T>(atom: Atom<T>) => T;
-declare const useSetGlobalState: <T>(atom: Atom<T>) => (value: T) => void;
-declare const RootState: FC<RootStateProps>;
-export { RootState, atom, emmiter, useGlobalState, useGlobalValue, useSetGlobalState, useFreeGlobalState, useGlobalUnSubscribe, };
+export {};
--- index.js
+++ index.js
@@ -1,83 +1,28 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.useGlobalUnSubscribe = exports.useFreeGlobalState = exports.useSetGlobalState = exports.useGlobalValue = exports.useGlobalState = exports.emmiter = exports.atom = exports.RootState = void 0;
-const react_1 = require("react");
+import { useLayoutEffect, useRef, useState } from "react";
 const emmiter = (all = new Map()) => ({
     all,
-    on: (type, handler) => {
-        const handlers = all.get(type);
-        handlers ? handlers.push(handler) : all.set(type, [handler]);
-    },
-    off: (type, handler) => {
-        const handlers = all.get(type);
-        handlers && handler ?
-            handlers.splice(handlers.indexOf(handler) >>> 0, 1) :
-            all.set(type, []);
-    },
-    emit: (type, evt) => {
-        let handlers = all.get(type);
-        handlers && evt && handlers
-            .slice().map((handler) => handler(evt));
-    }
-});
-exports.emmiter = emmiter;
-const context = {
+    on: (type, handler, handlers = all.get(type)) => handlers ? handlers.push(handler) : all.set(type, [handler]),
+    off: (type, handler, handlers = all.get(type)) => handlers && handler ? handlers.splice(handlers.indexOf(handler) >>> 0, 1) : all.set(type, []),
+    emit: (type, evt, handlers = all.get(type)) => handlers && evt && handlers.slice().map((handler) => handler(evt))
+}), context = {
     store: new Map(),
     emmit: emmiter()
-};
-const GlobalStateContext = (0, react_1.createContext)(context);
-const atom = (opt) => opt;
-exports.atom = atom;
-const useFreeGlobalState = (atom) => [useGlobalUnSubscribe(atom), useSetGlobalState(atom)];
-exports.useFreeGlobalState = useFreeGlobalState;
-const useGlobalState = (atom) => [useGlobalValue(atom), useSetGlobalState(atom)];
-exports.useGlobalState = useGlobalState;
-const useGlobalUnSubscribe = (atom) => {
-    const context = (0, react_1.useContext)(GlobalStateContext);
-    if (!context) {
-        throw new Error('This component must be used inside a <RootState> component.');
+}, stateAtom = (opt) => ({
+    key: opt.key,
+    default: opt.default,
+    get: () => context.store.get(opt.key) || opt.default,
+    set: (value) => context.store.set(opt.key, value),
+    sub: (handler) => {
+        context.emmit.on(opt.key, handler);
+        return () => context.emmit.off(opt.key, handler);
     }
-    if (!context.store.has(atom.key)) {
-        context.store.set(atom.key, atom.default);
-    }
-    const value = (0, react_1.useRef)(context.store.get(atom.key));
-    const handlerDispatch = (v) => {
-        value.current = v;
-    };
-    (0, react_1.useLayoutEffect)(() => {
-        context.emmit.on(atom.key, handlerDispatch);
-        return () => context.emmit.off(atom.key, handlerDispatch);
-    }, []);
+});
+export const atom = (opt) => stateAtom(opt), useGlobalValue = (state) => {
+    const [value, dispatch] = useState(state.get());
+    useLayoutEffect(() => state.sub(dispatch), []);
+    return value;
+}, useSetGlobalState = (state) => (value) => context.emmit.emit(state.key, value), useGlobalState = (state) => [useGlobalValue(state), useSetGlobalState(state)], useFreeGlobalState = (state) => [useGlobalUnSubscribe(state), useSetGlobalState(state)], useGlobalUnSubscribe = (state) => {
+    const value = useRef(context.store.get(state.key));
+    useLayoutEffect(() => state.sub((v) => { value.current = v; }), []);
     return value;
 };
-exports.useGlobalUnSubscribe = useGlobalUnSubscribe;
-const useGlobalValue = (atom) => {
-    const context = (0, react_1.useContext)(GlobalStateContext);
-    if (!context) {
-        throw new Error('This component must be used inside a <RootState> component.');
-    }
-    if (!context.store.has(atom.key)) {
-        context.store.set(atom.key, atom.default);
-    }
-    const value = context.store.get(atom.key);
-    const [state, dispatch] = (0, react_1.useState)(value);
-    (0, react_1.useLayoutEffect)(() => {
-        context.emmit.on(atom.key, dispatch);
-        return () => context.emmit.off(atom.key, dispatch);
-    }, []);
-    return state;
-};
-exports.useGlobalValue = useGlobalValue;
-const useSetGlobalState = (atom) => {
-    const context = (0, react_1.useContext)(GlobalStateContext);
-    if (!context) {
-        throw new Error('This component must be used inside a <RootState> component.');
-    }
-    if (!context.store.has(atom.key)) {
-        context.store.set(atom.key, atom.default);
-    }
-    return (value) => context.emmit.emit(atom.key, value);
-};
-exports.useSetGlobalState = useSetGlobalState;
-const RootState = ({ children }) => ((0, react_1.createElement)(GlobalStateContext.Provider, { value: context }, children));
-exports.RootState = RootState;
